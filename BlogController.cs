70f4b9fd6592b4edc0aacfd4e0aff0798dd75da5
Alparslan YÜCE <44805477+alparslanyuce@users.noreply.github.com>
2021-05-31 20:58:38 +0300

    Add files via upload

---
+++ BlogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Banzhow.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            //veritabanından bütün blog bilgileri al ve view üzerine gönder.
+            // kendisine gelen blog bilgilerini dinamik içerik üreten View yapısı bunu statik Html'e çevirir.
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
